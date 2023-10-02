6f14078560e4d384169cc3075be3a063ba86d12c
Siddhant043 <siddhant48311@gmail.com>
2023-10-03 02:54:25 +0530

    updated code

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { useSession } from "next-auth/react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import Profile from "@components/profile";
